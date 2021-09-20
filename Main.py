8711da7f8f535ce2379d65b76b5b0ba5eb6c60bd
cwake7960 <49754203+cwake7960@users.noreply.github.com>
2021-09-20 11:10:52 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,9 @@
-print("hello world")
+print("Welcome to happy fun game")
+name = input("What is your name ")
+age = input("what is your age ")
+print("hello ", name, "you are ", age ," years old")
+
+if int(age) >= 18:
+    print(name, " you are old enough")
+else:
+    print(name, " you are not old enough")
