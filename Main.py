794d2c200b1ef9abe5d5dbd81849a43b810699d6
cwake7960 <49754203+cwake7960@users.noreply.github.com>
2021-09-21 09:56:12 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,5 +5,21 @@ print("hello ", name, "you are ", age ," years old")
 
 if int(age) >= 18:
     print(name, " you are old enough")
+
+wants_to_play = input("Do you wnant to play? ").lower()
+
+if wants_to_play == "yes":
+    print("Lets play")
+
+    ans = input("Are you going to go left or right?(left/right)").lower()
+
+    if ans == "left":
+        print("left")
+
+    elif ans == "right":
+        print("right")
+
+    else: print("you suck dumbass")
+
 else:
     print(name, " you are not old enough")
